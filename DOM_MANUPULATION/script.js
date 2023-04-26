f774a5e80a07e4f9711253b5050a7bd47c43f896
Samrat Ghosh <101434286+samratghosh291@users.noreply.github.com>
2023-04-26 20:00:31 +0530

    Add files via upload

---
+++ DOM_MANUPULATION/script.js
@@ -0,0 +1,43 @@
+//DOM Manipulation
+
+
+
+
+
+
+//getElementById()
+
+const title = document.getElementById('main-heading');
+console.log(title);
+
+
+
+//getElementByClassName()
+
+
+const listItem = document.getElementsByClassName('list-items');
+
+console.log(listItem); 
+
+
+
+//getElementByTagName()
+
+const tagName = document.getElementsByTagName('ul');
+console.log(tagName);
+
+
+
+//querySelector()
+
+const container = document.querySelector('div');
+console.log(container);
+
+
+
+//querySelectorAll()
+
+const container1 = document.querySelectorAll('div');
+console.log(container1);
+
+
